0500bbd7bceabfeb911bc4fe9e09bb469e027582
AlexRecross <perekhrestalexandr@gmail.com>
2023-09-15 21:56:36 +0300

    Processing point
    CreateCourse component:
    add author functionality

--- src/components/Create courses/CreateCourse.jsx
+++ src/components/Create courses/CreateCourse.jsx
@@ -2,13 +2,17 @@ import React, { useState } from 'react';
 import Button from '../../common/Button';
 import toHoursAndMinutes from '../../helpers/pipeDurations';
 import { mockedAuthorsList } from '../../Constants';
+import { v4 as uuidv4 } from 'uuid';
 // import { mockedCoursesList } from '../../Constants';
 
 function CreateCourse() {
 	const [duration, setDuration] = useState(0);
+	const [authorsList, addAuthors] = useState(mockedAuthorsList);
 	function durationToHourse() {
 		return toHoursAndMinutes(duration);
 	}
+	// const authorsList = mockedAuthorsList;
+	const authors = mockedAuthorsList.map((o) => ({ ...o, selected: false }));
 	function Header() {
 		return (
 			<div className='createCourseHeader'>
@@ -27,6 +31,33 @@ function CreateCourse() {
 			</div>
 		);
 	}
+	function AddNewAuthor() {
+		function handleSubmit() {
+			const input = document.getElementById('InputAuthorsName');
+			if (/^[A-Z][A-Za-z]+\s+[A-Z][A-Za-z]/.test(input.value)) {
+				const authorName = input.value;
+				const newAuthor = { id: uuidv4(), name: authorName };
+				addAuthors(mockedAuthorsList.push(newAuthor));
+			} else {
+				console.log('Please enter your Fullname');
+			}
+		}
+
+		return (
+			<div>
+				<h2>Add author</h2>
+				<input
+					id='InputAuthorsName'
+					type='text'
+					name='name'
+					placeholder='Enter author name...'
+				/>
+				<br />
+				<button onClick={handleSubmit}>Add</button>
+			</div>
+		);
+	}
+
 	function Description() {
 		return (
 			<div className='descriptionCC '>
@@ -45,11 +76,11 @@ function CreateCourse() {
 		return (
 			<ul className='authorsListCC'>
 				<b>Authors</b>
-				{mockedAuthorsList.map((author) => {
+				{authors.map((author) => {
 					return (
 						<li key={author.id}>
 							<span>{author.name}</span>
-							<Button name='Add author'></Button>
+							<Button name='Add author' />
 						</li>
 					);
 				})}
@@ -57,46 +88,33 @@ function CreateCourse() {
 		);
 	}
 	return (
-		<form className='createCourseForm'>
-			<Header />
-			<Description />
-			<div className='specificationCC'>
-				<div className='durationAndAuthorCC'>
+		<div>
+			<form className='createCourseForm'>
+				<Header />
+				<Description />
+				<div className='specificationCC'>
 					<div className='durationAndAuthorCC'>
-						<h2>Add author</h2>
-						<label htmlFor='authorNameCC'>Author name</label>
-						<input
-							type='text'
-							id='authotNameCC'
-							name='name'
-							placeholder='Enter author name...'
-						/>
-						<Button
-							name='Create author'
-							function={() => {
-								console.log('Author created');
-							}}
-						/>
-					</div>
-					<div className='durationAndAuthorCC'>
-						<div className='inputTxtCC'>
-							<h2>Duration</h2>
-							<label htmlFor='durationCC'>Duration</label>
-							<input
-								type='number'
-								id='durationCC'
-								placeholder='Enter duration in minutes'
-								onChange={(event) => setDuration(event.target.value)}
-							/>
-							<b>Duration: {durationToHourse()}</b>
+						<AddNewAuthor />
+						<div className='durationAndAuthorCC'>
+							<div className='inputTxtCC'>
+								<h2>Duration</h2>
+								<label htmlFor='durationCC'>Duration</label>
+								<input
+									type='number'
+									id='durationCC'
+									placeholder='Enter duration in minutes'
+									onChange={(event) => setDuration(event.target.value)}
+								/>
+								<b>Duration: {durationToHourse()}</b>
+							</div>
 						</div>
 					</div>
+					<div className='authorsListCC'>
+						<AviableAuthors />
+					</div>
 				</div>
-				<div className='authorsListCC'>
-					<AviableAuthors />
-				</div>
-			</div>
-		</form>
+			</form>
+		</div>
 	);
 }
 export default CreateCourse;
