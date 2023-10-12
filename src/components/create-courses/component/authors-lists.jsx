7d622ea58a86f3c26cbd1194526bd5e48783a507
AlexRecross <perekhrestalexandr@gmail.com>
2023-10-12 14:56:10 +0300

    <authors-map> turn to new separate component, and add all needed functionality. need add memoisation in this component - processing point
    need to finish <formState> and add functionality taking into account all changes

---
+++ src/components/create-courses/component/authors-lists.jsx
@@ -0,0 +1,133 @@
+import React from "react";
+import Button from "../../../common/button";
+
+export default function AuthorsLists({ className, authorsList, setAuthorsList }) {
+
+	const authorsNotSelected = authorsList.filter(({ selected }) => !selected);
+	const authorsSelected = authorsList.filter(({ selected }) => selected);
+
+	function selectToggle(authorId) {
+		const newArray = authorsList.filter((author) => author.id !== authorId);
+		const author = authorsList.find(author => author.id === authorId);
+		author.selected === true ?	author.selected = false : author.selected = true;
+		newArray.push(author)
+		setAuthorsList(newArray)
+	}
+
+	function nonSelectedList() {
+		if(authorsNotSelected.length >= 1) {
+			return (
+				<ul>
+					<b>Available authors:</b>
+					{authorsNotSelected.map((author) => (
+						<li key={author.id}>
+							<span>{author.name}</span>
+							<Button
+								type='button'
+								onClick={() => selectToggle(author.id)}
+							>
+								Add author
+							</Button>
+						</li>
+					))}
+				</ul>
+			)
+		} else {
+			return (
+				<ul>
+					<li><b>Available authors empty</b></li>
+				</ul>
+			)
+		}
+	}
+
+	function selectedList() {
+		if(authorsSelected.length >= 1) {
+			return (
+				<ul>
+					<b>Selected authors:</b>
+					{authorsSelected.map((author) => (
+						<li key={author.id}>
+							<span>{author.name}</span>
+							<Button
+								type='button'
+								onClick={() => selectToggle(author.id)}
+							>
+								Add author
+							</Button>
+						</li>
+					))}
+				</ul>
+			)
+		} else {
+			return (
+				<ul>
+					<li><b>Selected authors empty</b></li>
+				</ul>
+			)
+		}
+	}
+
+	// authorsNotSelected.length <= 0 ? console.log('authorsNotSelected', true, authorsNotSelected) : console.log('authorsNotSelected', false, authorsNotSelected)
+	// authorsSelected.length <= 1 ? console.log('authorsSelected', true, authorsSelected) : console.log('authorsSelected', false, authorsSelected)
+
+	return(
+		<div className={className}>
+			{nonSelectedList()}
+			{selectedList()}
+		</div>
+	)
+}
+
+
+
+
+// if (authors.length === 0) {
+// 	return (
+// 		<div>
+// 			<h2>{listName}</h2>
+// 			<br />
+// 			<span>Author list is empty</span>
+// 		</div>
+// 	);
+// } else {
+// 	return (
+// 		<div>
+// 			<ul>
+// 				<h2>{listName}</h2>
+// 				{authors.map((author) => {
+// 					function authorSelectToggle() {
+// 						const newArray = authorsList.filter((obj) => obj.id !== author.id);
+// 						author.selected === true ?	author.selected = false : author.selected = true;
+// 						newArray.push(author)
+// 						setAuthorsList(newArray)
+// 					}
+// 					return (
+// 						<li key={author.id}>
+// 							<span>{author.name}</span>
+// 							<Button type='button' onClick={authorSelectToggle}>{buttonName}</Button>
+// 						</li>
+// 					);
+// 				})}
+// 			</ul>
+// 		</div>
+// 	);
+// }
+
+//
+// <div className={styles.authorsListCC}>
+// 	<AuthorsMap
+// authorsList={authorsList}
+// setAuthorsList={setAuthorsList}
+// authors={authorsNotSelected}
+// listName='Authors'
+// buttonName='Add author'
+// 	/>
+// 	<AuthorsMap
+// authorsList={authorsList}
+// setAuthorsList={setAuthorsList}
+// authors={authorsSelected}
+// listName='Course authors'
+// buttonName='Delete author'
+// 	/>
+// 	</div>
--- src/components/create-courses/create-course.jsx
+++ src/components/create-courses/create-course.jsx
@@ -1,25 +1,26 @@
-import React, { useState, useEffect } from 'react';
+import React, {useEffect, useState} from 'react';
 import styles from './create-course.module.css'
 import Button from '../../common/button';
 import Duration from "../duration/durations";
 import Input from "../../common/input";
-import AuthorsMap from "./component/authors-map";
+import AuthorsLists from "./component/authors-lists";
 import AddNewAuthor from "./component/create-new-author";
 import { v4 as uuidv4 } from 'uuid';
 
 function CreateCourse({authors, addAuthor, addCourse, goToCourses}) {
 
+	// AuthorsLists states and functionality
+	const [authorsList, setAuthorsList] = useState(authors);
+
+	useEffect( () => {
+		setAuthorsList(authors.map(author => ({ ...author, selected: false })))
+	}, [authors])
+
+
 	//"Add New Author" functionality
 	function addNewAuthorToList(newAuthor) {
 		newAuthor !== undefined ? addAuthor(newAuthor) : console.log('newAuthor false',newAuthor)
 	}
-	// Authors list selected and nonSelected template and functionality
-	const [authorsList, setAuthorsList] = useState(authors);
-	const authorsNotSelected = authorsList.filter(({ selected }) => !selected);
-	const authorsSelected = authorsList.filter(({ selected }) => selected);
-	useEffect( () => {
-		setAuthorsList(authors.map(author => ({ ...author, selected: false })))
-	}, [authors])
 
 	// Form States, data and functionality
 	const [formState, setFormState] = useState({
@@ -49,11 +50,11 @@ function CreateCourse({authors, addAuthor, addCourse, goToCourses}) {
 		})
 	}
 
-	function setDuration(event) {
+	function setDuration(value) {
 		setFormState(prevState => {
 			return {
 				...prevState,
-				duration: parseInt(event.target.value, 10)
+				duration: value
 			}
 		})
 	}
@@ -61,18 +62,18 @@ function CreateCourse({authors, addAuthor, addCourse, goToCourses}) {
 	// const [description, setDescription] = useState('');
 	// const [duration, setDuration] = useState(0);
 
-	function getAuthorsId() {
-		const authorsId = []
-		if(authorsSelected.length < 2){
-			return alert('course should have 2 authors at least')
-		}else{
-			for(let i = 0; i < authorsSelected.length; i++){
-				let id = authorsSelected[i].id;
-				authorsId.push(id)
-			}
-		}
-		return authorsId
-	}
+	// function getAuthorsId() {
+	// 	const authorsId = []
+	// 	if(authorsSelected.length < 2){
+	// 		return alert('course should have 2 authors at least')
+	// 	}else{
+	// 		for(let i = 0; i < authorsSelected.length; i++){
+	// 			let id = authorsSelected[i].id;
+	// 			authorsId.push(id)
+	// 		}
+	// 	}
+	// 	return authorsId
+	// }
 
 	function getCourseFormData() {
 		const creationDate = new Date().toLocaleString().slice(0,10).replace(/\./g,'/')
@@ -82,35 +83,35 @@ function CreateCourse({authors, addAuthor, addCourse, goToCourses}) {
 			// description: description,
 			creationDate: creationDate,
 			// duration: duration,
-			authors: getAuthorsId()
+			// authors: getAuthorsId()
 		}
 	}
 
-	function validateCourseFormData() {
-		// if( title === '') {
-		// 	alert('Field "Title" is not filled in!')
-		// 	return false
-		// }
-		// if( description === '') {
-		// 	alert('Field "Description" is not filled in!')
-		// 	return false
-		// }
-		if( authorsSelected.length < 2) {
-			alert('The course must include at least two authors!')
-			return false
-		}
-		// if(duration <= 0) {
-		// 	alert('Field "Duration" is not filled in!')
-		// 	return false
-		// }
-		return true
-	}
+	// function validateCourseFormData() {
+	// 	if( title === '') {
+	// 		alert('Field "Title" is not filled in!')
+	// 		return false
+	// 	}
+	// 	if( description === '') {
+	// 		alert('Field "Description" is not filled in!')
+	// 		return false
+	// 	}
+	// 	if( authorsSelected.length < 2) {
+	// 		alert('The course must include at least two authors!')
+	// 		return false
+	// 	}
+	// 	if(duration <= 0) {
+	// 		alert('Field "Duration" is not filled in!')
+	// 		return false
+	// 	}
+	// 	return true
+	// }
 
 	function handleSubmit(e){
 		e.preventDefault();
-		if(!validateCourseFormData()){
-			return alert('Form not valid');
-		}
+		// if(!validateCourseFormData()){
+		// 	return alert('Form not valid');
+		// }
 		const course = getCourseFormData();
 		addCourse(course)
 		goToCourses()
@@ -166,27 +167,16 @@ function CreateCourse({authors, addAuthor, addCourse, goToCourses}) {
 							inputId='durationCC'
 							name='Duration'
 							placeholder='Enter duration in minutes'
-							onChange={event => setDuration(event)}
+							onChange={event => setDuration(parseInt(event.target.value, 10))}
 						/>
 						<Duration value={formState.duration} />
 					</div>
 				</div>
-				<div className={styles.authorsListCC}>
-					<AuthorsMap
-						authorsList={authorsList}
-						setAuthorsList={setAuthorsList}
-						authors={authorsNotSelected}
-						listName='Authors'
-						buttonName='Add author'
-					/>
-					<AuthorsMap
-						authorsList={authorsList}
-						setAuthorsList={setAuthorsList}
-						authors={authorsSelected}
-						listName='Course authors'
-						buttonName='Delete author'
-					/>
-				</div>
+				<AuthorsLists
+					className={styles.authorsListCC}
+					authorsList={authorsList}
+					setAuthorsList={setAuthorsList}
+				/>
 			</div>
 		</form>
 	);
